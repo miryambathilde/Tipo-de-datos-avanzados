d86defbbb0a4fc68efcc2a75511fdb980ef7a5ad
Miryam <mbathilde@gmail.com>
2021-10-13 11:16:07 +0200

    array string unidimensional - declaracion y recorrer con bucles

--- src/TiposDeDatosAvanzados/Array.java
+++ src/TiposDeDatosAvanzados/Array.java
@@ -1,15 +1,40 @@
 package TiposDeDatosAvanzados;
 
+import java.io.PrintStream;
+
 public class Array {
     public static void main(String[] args) {
 
-        // ARRAY DE ENTEROS
+        // ARRAY DE STRINGS
 
         // DECLARAMOS E INICIALIZAMOS EL ARRAY
-        int arrayUno[] = { 1, 2, 3, 4, 5};
-        // RECORREMOS EL ARRAY
-        for (int i : arrayUno) {
-            System.out.println(i);
+        String nombres[] = {
+                "Pepe",
+                "Juan",
+                "Miryam",
+        };
+
+        // PROPIEDAD LENGTH
+        System.out.println("Longitud del array: " + nombres.length);
+
+        // MANERAS DE RECORRER UN ARRAY
+
+        // 1. BUCLE FOR EACH - forma corta
+        for (String nombre : nombres) {
+            System.out.println("Nombre actual con for each: " + nombre);
         }
+
+        // 2. FORMA LARGA: BUCLE FOR INT
+        // Tiene la ventaja de que obtenemos el i, el indice o posición dentro del array, ya que usamos el contador con el i
+
+        String ultimoNombre = "";
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println("Nombre actual con for int: " + nombres[i] + ", en posición: " + i);
+            ultimoNombre = nombres[i];
+        };
+
+        System.out.println("El último nombre del array es: " + ultimoNombre);
+
     }
 }
